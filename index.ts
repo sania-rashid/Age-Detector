88a6971e67095ffd333cb3c345672db13131699d
Sania Rashid <officialsania113@gmail.com>
2024-04-30 01:45:29 +0500

    age detect

---
+++ index.ts
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+let number = 18;
+let answer = await inquirer.prompt({
+  name: "age",
+  type: "number",
+  message: "Enter your age",
+});
+if (answer.age < number) {
+  console.log("You are under age.");
+} else {
+  console.log("You are eligible.");
+}
+let Answer = await inquirer.prompt([
+    {
+  name: "your",
+  message: "Select your course.",
+  type: "list",
+  choices: ["python", "typescript", "C++", "SQL"],
+}])
+console.log("Congratulations! you are eligible and selected in this course.")
